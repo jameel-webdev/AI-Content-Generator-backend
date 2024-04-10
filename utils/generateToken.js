97eeaa193f7743b6c4095640ce8d5f1283a671e4
Jameel Ahmed <jameel.webdev@gmail.com>
2024-04-10 16:21:58 +0530

    sameSite: none

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
+    expiresIn: "30d",
   });
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
-    maxAge: 24 * 60 * 60 * 1000,
+    sameSite: "none",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
 
